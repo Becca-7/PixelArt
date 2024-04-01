61c5dc09c9f467f915b784acd8828c92f42bd149
beccaGoGoSkis <rebecca_white25@milton.edu>
2024-04-01 13:21:43 -0400

    Final Pixel Art

---
+++ src/Rectangles.java
@@ -0,0 +1,37 @@
+import java.awt.*;
+
+public class Rectangles {
+
+
+        public int xpos;                //the x position
+        public int ypos;                //the y position
+        public int width;
+        public int height;
+        public boolean isAlive;            //a boolean to denote if the hero is alive or dead.
+        public Rectangle rec;
+        public Image pic;
+        public int colorCode;
+
+
+
+
+
+    public Rectangles(int pXpos, int pYpos, Image picParameter, int pColorCode) {
+
+            xpos = pXpos;
+            ypos = pYpos;
+            width = 50;
+            height = 50;
+
+            pic = picParameter;
+            isAlive = true;
+            rec = new Rectangle(xpos, ypos, width, height);
+            colorCode=pColorCode;
+
+        } // constructor
+
+
+    }
+
+
+
